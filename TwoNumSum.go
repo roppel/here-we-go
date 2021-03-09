a27013a9d76a637aee03898f6d8f4ded545ef40e
Zack Roppel <roppel@gmail.com>
2021-03-08 23:39:31 -0800

    Two Number Sum with sort. O(nlogN) time, O(1) space

--- TwoNumSum.go
+++ TwoNumSum.go
@@ -5,6 +5,14 @@ import (
 	"sort"
 )
 
+/*
+todo try without sort (with map), play around with project setup
+to run from main:
+	ar := []int{3, 5, -4, 8, 11, 1, -1, 6}
+	result := TwoNumberSumSort(ar, 10)
+	fmt.Printf("%v\n", result)
+*/
+
 func TwoNumberSumSort(ar []int, target int) []int {
 
 	sort.Ints(ar)
--- hello.go
+++ hello.go
@@ -7,7 +7,6 @@ import (
 func main() {
 	ar := []int{3, 5, -4, 8, 11, 1, -1, 6}
 	result := TwoNumberSumSort(ar, 10)
-
 	fmt.Printf("%v\n", result)
 
 }
